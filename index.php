01c385f0e9fa5e8c7f5dec162e49af227372cbe2
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-28 14:05:03 +0100

    Exo 174 ok

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 // Premiere ligne
+session_start();
 
+$_SESSION['color'] = 'red';
 
 
 
---
+++ index2.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+echo "Affichage de la global Session : " .$_SESSION['red'];
